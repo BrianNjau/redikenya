c14999e41f4e9a13f6eb7fdd839655dd4fe2885f
Brian <brian.wahinya.njau@huawei.com>
2024-01-20 16:25:00 +0300

    Add Unitprice on scatterchart

--- src/Components/MultiRangeSlider.jsx
+++ src/Components/MultiRangeSlider.jsx
@@ -1,94 +1,49 @@
-import React, { useCallback, useEffect, useState, useRef, memo } from "react";
+import React, { useState } from "react";
 
-// Libraries
-import PropTypes from "prop-types";
 
-// css
-import "../Assets/scss/components/_multirangeslider.scss"
+import { InputNumber, Slider } from "antd";
 
 const MultiRangeSlider = ({ min, max, onChange }) => {
-    const [minVal, setMinVal] = useState(min);
-    const [maxVal, setMaxVal] = useState(max);
-    const minValRef = useRef(min);
-    const maxValRef = useRef(max);
-    const range = useRef(null);
+    const [sliderValMin, setSliderValMin] = useState(min);
+    const [sliderValMax, setSliderValMax] = useState(max);
 
-    // Convert to percentage
-    const getPercent = useCallback(
-        (value) => Math.round(((value - min) / (max - min)) * 100),
-        [min, max]
-    );
+    //my function
+    const minInputRangeSliderHandler = (val) => {
+        
+        setSliderValMin(val);
+        
+    }
+    //my function
+    const maxInputRangeSliderHandler = (val) => {
+        
+        setSliderValMax(val);
+        
+    }
+    const rangeSliderHandler = (val) => {
+       
+        setSliderValMin(val[0]);
+        setSliderValMax(val[1]);
+    }
 
-    // Set width of the range to decrease from the left side
-    useEffect(() => {
-        const minPercent = getPercent(minVal);
-        const maxPercent = getPercent(maxValRef.current);
-
-        if (range.current) {
-            range.current.style.left = `${minPercent}%`;
-            range.current.style.width = `${maxPercent - minPercent}%`;
-        }
-    }, [minVal, getPercent]);
-
-    // Set width of the range to decrease from the right side
-    useEffect(() => {
-        const minPercent = getPercent(minValRef.current);
-        const maxPercent = getPercent(maxVal);
-
-        if (range.current) {
-            range.current.style.width = `${maxPercent - minPercent}%`;
-        }
-    }, [maxVal, getPercent]);
-
-    // Get min and max values when their state changes
-    useEffect(() => {
-        onChange && onChange({ min: minVal, max: maxVal });
-    }, [minVal, maxVal, onChange]);
+    
 
     return (
-        <div className="multi-range-slider">
-            <input
-                type="range"
-                min={min}
-                max={max}
-                value={minVal}
-                onChange={(event) => {
-                    const value = Math.min(Number(event.target.value), maxVal - 1);
-                    setMinVal(value);
-                    minValRef.current = value;
-                }}
-                className="thumb thumb--left"
-                style={{ zIndex: minVal > max - 100 && "5" }}
-            />
-            <input
-                type="range"
-                min={min}
-                max={max}
-                value={maxVal}
-                onChange={(event) => {
-                    const value = Math.max(Number(event.target.value), minVal + 1);
-                    setMaxVal(value);
-                    maxValRef.current = value;
-                }}
-                className="thumb thumb--right"
-            />
-            <div className="slider">
-                <div className="slider__track" />
-                <div ref={range} className="slider__range" />
-            </div>
-            <div className="price-filter-details">
-                <button type="submit" className="btn-filter btn">Filter</button>
-                <span className="price-filter-amount">
-                    <label className="filter-value" htmlFor="price-amount">Price: Ksh.{minVal} - Ksh.{maxVal}</label>
-                </span>
-            </div>
+        <div>
+        <Slider range defaultValue={[min, max]} min={min} max={max} onChange={rangeSliderHandler} value={[sliderValMin, sliderValMax]} />
+        <InputNumber 
+        prefix="Ksh. "
+        style={{width:"46%"}}
+         className="thumb thumb--left mr-1 " formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+         parser={(value) => value.replace(/\$\s?|(,*)/g, '')} value={sliderValMin} onChange={minInputRangeSliderHandler} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+        <InputNumber
+          prefix="Ksh. " 
+          style={{width:"46%"}}
+         className="thumb thumb--right " formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+         parser={(value) => value.replace(/\$\s?|(,*)/g, '')} value={sliderValMax} onChange={maxInputRangeSliderHandler} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
         </div>
     );
 };
 
-MultiRangeSlider.propTypes = {
-    min: PropTypes.number.isRequired,
-    max: PropTypes.number.isRequired
-};
 
-export default memo(MultiRangeSlider);
+
+export default MultiRangeSlider;
