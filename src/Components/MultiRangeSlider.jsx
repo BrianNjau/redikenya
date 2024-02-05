5729b466a9eecb9627953b4371d429ca4b4e16fe
Brian <brian.wahinya.njau@huawei.com>
2024-02-05 20:20:28 +0300

    Invest Features

--- src/Components/MultiRangeSlider.jsx
+++ src/Components/MultiRangeSlider.jsx
@@ -11,31 +11,35 @@ const MultiRangeSlider = ({ min, max, onChange }) => {
     const minInputRangeSliderHandler = (val) => {
         
         setSliderValMin(val);
+
         
     }
     //my function
     const maxInputRangeSliderHandler = (val) => {
         
         setSliderValMax(val);
-        
+       
     }
     const rangeSliderHandler = (val) => {
        
         setSliderValMin(val[0]);
         setSliderValMax(val[1]);
+        
     }
 
     
 
     return (
         <div>
-        <Slider range defaultValue={[min, max]} min={min} max={max} onChange={rangeSliderHandler} value={[sliderValMin, sliderValMax]} />
+        <Slider range defaultValue={[min, max]} min={min} max={max} onAfterChange={onChange} onChange={rangeSliderHandler}  value={[sliderValMin, sliderValMax]} />
         <InputNumber 
+        disabled
         prefix="Ksh. "
         style={{width:"46%"}}
          className="thumb thumb--left mr-1 " formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
          parser={(value) => value.replace(/\$\s?|(,*)/g, '')} value={sliderValMin} onChange={minInputRangeSliderHandler} />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
         <InputNumber
+         disabled
           prefix="Ksh. " 
           style={{width:"46%"}}
          className="thumb thumb--right " formatter={(value) => `${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
--- src/Pages/Invest.jsx
+++ src/Pages/Invest.jsx
@@ -2,29 +2,31 @@ import React, { useContext, useEffect, useState } from 'react'
 import GlobalHeader from '../Components/GlobalHeader'
 import { Col, Container, Row } from 'react-bootstrap'
 import Typed from 'react-typed';
-
 import { fadeIn } from '../Functions/GlobalAnimations';
 import { Link } from 'react-router-dom';
 import MultiRangeSlider from '../Components/MultiRangeSlider';
-import { Select, Skeleton } from 'antd';
+import { Checkbox, Select, Skeleton, Result, FloatButton, Button, Modal, List, Space, Spin } from 'antd';
 import GlobalContext from '../Context/Context';
 import { Supabase } from '../Functions/SupabaseClient';
+import Buttons from '../Components/Buttons';
+import { DollarOutlined, ExclamationCircleOutlined, HomeFilled, HomeOutlined, MoneyCollectFilled, MonitorOutlined } from '@ant-design/icons';
+import { type } from '@testing-library/user-event/dist/type';
+
 const Invest = () => {
-
-const options = [];
-
-for (let i = 10; i < 36; i++) {
-  options.push({
-    value: i.toString(36) + i,
-    label: i.toString(36) + i,
-  });
-}
 const [loadingPropData, setLoadingPropData] = useState([]);
 const [loadingPropOverview, setLoadingPropOverview] = useState([]);
 const [propData, setPropData] = useState([]);
 const [propOverview, setPropOverview] = useState([]);
+const [selectedMarketPrice, setSelectedMarketPrice] = useState([]);
+const [selectedRentPrice, setSelectedRentPrice] = useState([]);
+const [selectedLocation, setSelectedLocation] = useState([]);
+const [selectedTypology, setSelectedTypology] = useState([]);
+const [filterSearchLoading, setFilterSearchLoading] = useState(false);
+const [filterSearchResults, setFilterSearchResults] = useState([]);
+const [isConfirmSearchModalOpen, setIsConfirmSearchModalOpen] = useState(false);
 
 const { setHeaderHeight } = useContext(GlobalContext);
+
 
 useEffect(()=>{
 setHeaderHeight(120);
@@ -72,10 +74,137 @@ const rents = propData.map(val =>  parseFloat(val["Rent"].replace(/,/g, '')));
 const locations = [...new Set(propOverview.map(val =>  val["Location"]))];
 
 
-console.log(locations)
+const handleMarketPricePicker =  (marketPrices) => {
+  setSelectedMarketPrice(marketPrices);
+}
+const handleMarketPriceNumberPicker =  (marketPrices) => {
+  setSelectedMarketPrice(marketPrices);
+}
+const handleRentPricePicker =  (rentPrices) => {
+  setSelectedRentPrice(rentPrices)
+}
+const handleLocationPicker = (locations) =>{
+  setSelectedLocation(locations)
+}
+const handleTypologyPicker = (typologies) =>{
+  setSelectedTypology(typologies)
+}
+
+
+function filterByValue(data, selectedPrice, type){
+    try{
+      const isValidPrice = (marketPrice)=> {
+        return !isNaN(parseFloat(marketPrice)) && isFinite(marketPrice);
+        };
+        const filterPriceRange = (result, property) => {
+        const marketPrice = property[type];
+        if((type!=="Rent" ) && (!marketPrice || !isValidPrice(marketPrice.split(" ")[1].trim().replace(/,/g, '')))){
+          result.discarded.push(property) // add to discarded items
+          return false;  
+        };
+        const priceValue = type==="Rent"? parseFloat(marketPrice.replace(/,/g,'')): parseFloat(marketPrice.split(" ")[1].trim().replace(/,/g,''));
+        if(selectedPrice.length!==0 && (priceValue<selectedPrice[0] || priceValue >selectedPrice[1])){
+          result.discarded.push(property) // add to discarded items
+          return result;
+        }; 
+        result.filtered.push(property);
+        return result;
+      };
+       const initialResult = {filtered: [], discarded: []};
+       const {filtered, discarded }= data.reduce(filterPriceRange, initialResult)
+       return {filtered, discarded};
+    }catch(err){
+      console.log(err);
+    }
+}
+
+function filterByLocation(data, selectedLocation, lookUpData){
+  try{
+    if(selectedLocation.length===0){
+      return data; 
+    }
+    const filterLocations = (result) => {
+    const propertyID = result["PropertyID"];
+    const foundLocation = lookUpData.find((a) => a["Property ID"] === propertyID && selectedLocation.includes(a["Location"]));
+    console.log(foundLocation)
+    if(foundLocation){
+    return result
+    }
+    return false 
+    } 
+    const filteredResults = data.filter(filterLocations);
+    return filteredResults
+
+  }catch(err){
+    console.log(err)
+  }
+}
+
+function filterByTypology(data, typologies){
+  try{
+
+    if(typologies.length===0){
+      return data
+    }
+
+    const filteredResults = data.filter((property)=> 
+    typologies.includes(property["Typology "]));
+
+    return filteredResults;
+
+
+  }catch(err){
+    console.log(err)
+  }
+}
+
+
+
+ function filterProperties(){
+  try {
+    setFilterSearchLoading(true)
+    const filterByMarketPrice = filterByValue(propData,selectedMarketPrice,'Market Price');// filter by marketprice
+    console.log("MarketPrice result",filterByMarketPrice);
+    const filterByRent = filterByValue(filterByMarketPrice.filtered, selectedRentPrice, 'Rent');
+    const locationFiltered =  filterByLocation(filterByRent.filtered, selectedLocation, propOverview);
+    console.log("FILTERED WITH LO",locationFiltered)
+    const typologyFiltered = filterByTypology(locationFiltered, selectedTypology);
+    setFilterSearchResults(typologyFiltered);
+    setFilterSearchLoading(false)
+  } catch (error) {
+    setFilterSearchLoading(false);
+    
+  }
+}
+
+const showConfirmModal = () => {
+  setIsConfirmSearchModalOpen(true);
+};
+const handleOkSearch = () => {
+ filterProperties();
+ setIsConfirmSearchModalOpen(false);
+};
+
+const handleCancelSearch = () => {
+  setIsConfirmSearchModalOpen(false);
+};
+
+
+const IconText = ({icon,text}) => {
+
+  return(
+  <Space>
+    {icon}
+    {text}
+  </Space>
+);
+
+}
+
 
   return (
     <div>
+      
         <GlobalHeader theme="light" />
        {/* TO DO Make this section a component */}
         <section className="bg-lightgray py-[25px]">
@@ -101,6 +230,12 @@ console.log(locations)
        </Row>
      </Container>
    </section>
+   <FloatButton
+      icon={<MonitorOutlined />}
+      type="default"
+      badge={{ dot: true }}
+     
+    />
 
      {/* Section Start */}
      <section className="shopping-right-left-sidebar pt-0 py-[130px] lg:py-[90px] md:py-[75px] sm:py-[50px] mt-8">
@@ -109,6 +244,77 @@ console.log(locations)
                         <Col lg={9} md={8} className="pl-[55px] md:pl-[15px] sm:mb-[30px] order-md-2 order-1 sm:px-0">
 
                             {/* List data here  */}
+
+                      { 
+                          filterSearchResults.length===0? <Result style={{right:"50%", bottom:"50%"}} status="404" title="Search to uncover investment insights" subTitle="1 search = 1 token"/>:
+                                  <div>
+                                  <div>
+                                  <span className='float-right text-sm mb-12'>Found results: {filterSearchResults.length}</span>
+                                  <br />
+                                  <hr />
+                                  </div>
+
+
+                                  <List 
+                                  className='mt-8'
+                                  itemLayout='vertical'
+                                  size='large'
+                                  pagination={{
+                                    onChange: (page)=>{
+                                      console.log(page);
+                                    },
+                                    pageSize: 8
+                                  }}
+                                  dataSource={filterSearchResults}
+                                  footer={
+                                    <div>
+                                      <Typed className="font-semibold text-[#3eb489]"
+            strings={[ "PDI Marketplace Kenya","Property Development And Investments", "Property Data And Insights"]}
+            typeSpeed={6}
+            backSpeed={16}
+            loop
+            showCursor
+            cursorChar="|"/>
+                                    </div>
+                                  }
+                                  renderItem={(item)=>(
+                                    <List.Item key={item['Property Key']} 
+                                     actions={[
+                                      <IconText icon={<DollarOutlined />} text={`${item["Market Price"]}`} key={"market-price-icon"}/>,
+                                      <IconText icon={<HomeOutlined />} text={`${item["Typology "]}`} key={"home-price-icon"}/>,
+                                     ]}
+                                    //  extra={
+                                    //   <div>
+                                    //   <span className='text-xs'>Typology: {item["Typology "]}</span>
+
+                                    //   </div>
+
+                                    // }
+                                    >
+
+               <List.Item.Meta
+                  // avatar={<Avatar src={item.avatar} />}
+                 title={<span>{
+                  propOverview.find(a => a["Property ID"] === item["PropertyID"]) ? propOverview.find(a => a["Property ID"] === item["PropertyID"])["Name"] : <Spin/>        
+                  }</span>}
+                 description={propOverview.find(a => a["Property ID"] === item["PropertyID"]) ? propOverview.find(a => a["Property ID"] === item["PropertyID"])["Location"]: <Spin/>}
+                />
+                <div>
+                  <ul>
+                    <li>Rent: {item["Rent"]}</li>
+                    <li>Rental Yield: {item["Rental Yield"]}</li>
+                  </ul>
+               
+                </div>
+                                    </List.Item>
+                                    
+                                  )}
+                                  /> 
+                                       
+                                       
+                                  </div>
+                            }
+                       
                             {/* <ShopWide filter={false} grid="grid grid-3col xl-grid-4col lg-grid-3col md-grid-2col sm-grid-2col xs-grid-1col gutter-large" data={shopWideData} /> */}
                         </Col>
                         <div className="col col-lg-3 col-md-4 shopping-sidebar inline-block order-md-1 order-2" {...fadeIn}>
@@ -120,7 +326,7 @@ console.log(locations)
                                   loadingPropData ? <Skeleton active paragraph={{rows:1}}/>:<MultiRangeSlider
                                   min={0}
                                   max={Math.max(...marketPrices)}
-                                  onChange={({ min, max }) => (`min = ${min}, max = ${max}`)}
+                                  onChange={handleMarketPricePicker}
                               />
                                 }
                                 
@@ -132,7 +338,7 @@ console.log(locations)
                                   loadingPropData ? <Skeleton active paragraph={{rows:1}}/>:<MultiRangeSlider
                                   min={0}
                                   max={Math.max(...rents)}
-                                  onChange={({ min, max }) => (`min = ${min}, max = ${max}`)}
+                                  onChange={handleRentPricePicker}
                               />
                                 }
                             </div>
@@ -140,7 +346,7 @@ console.log(locations)
                             <div className="border-b border-mediumgray pb-12 mb-12 relative">
                                 <span className="shop-title relative font-serif font-medium text-darkgray block mb-[20px]">Filter By Location</span>
 
-                                <Select mode="multiple" size={"large"} placeholder="Please select" style={{width: '100%',}} options={locations.map((a) =>{return{value:a,label:a,}})}/>
+                                <Select loading={loadingPropOverview} mode="multiple" size={"large"} placeholder="Please select" style={{width: '100%',}} onChange={handleLocationPicker} options={locations.map((a) =>{return{value:a,label:a,}})}/>
                                 {/* <ul className="list-style filter-color">
                                     <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} target="_blank" href="#black"><span className="product-cb paroduct-color-cb" style={{ backgroundColor: "#363636" }}></span>Carbon black</a><span className="item-qty">25</span></li>
                                     <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} target="_blank" href="#blue"><span className="product-cb paroduct-color-cb" style={{ backgroundColor: "#657fa8" }}></span>Prussian blue</a><span className="item-qty">03</span></li>
@@ -153,37 +359,49 @@ console.log(locations)
                         
 
 
-                            <div className="border-b border-mediumgray pb-12 mb-12 relative">
+                            <div className="border-b border-mediumgray pb-12 mb-8 relative">
                                 <span className="shop-title relative font-serif font-medium text-darkgray block mb-[20px]">Filter By Typology</span>
                                 <ul className="list-style filter-category">
-                                    <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} href="#shirt"><span className="product-cb product-category-cb"></span>Studio</a>
-                                    {/* <span className="item-qty">25</span> */}
+                                  
+                                  <Checkbox.Group onChange={handleTypologyPicker}>  
+                                    <li>
+                                        <Checkbox value="Studio">Studio</Checkbox>
                                     </li>
-                                    <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} href="#bags"><span className="product-cb product-category-cb"></span>1 Bedroom</a>
-                                    {/* <span className="item-qty">05</span> */}
+                                    <li>
+                                    <Checkbox value="1BR">1 Bedroom</Checkbox>
                                     </li>
-                                    <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} href="#shirt"><span className="product-cb product-category-cb"></span>2 Bedroom</a>
-                                    {/* <span className="item-qty">36</span> */}
+                                    <li>
+                                    <Checkbox value="2BR">2 Bedroom</Checkbox>
                                     </li>
-                                    <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} href="#t-shirt"><span className="product-cb product-category-cb"></span>3 Bedroom</a>
-                                    {/* <span className="item-qty">05</span> */}
+                                    <li>
+                                    <Checkbox value="3BR">3 Bedroom</Checkbox>
                                     </li>
-                                    <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} href="#skirts"><span className="product-cb product-category-cb"></span>4 Bedroom</a>
-                                    {/* <span className="item-qty">09</span> */}
+                                    <li>
+                                    <Checkbox value="4BR">4 Bedroom</Checkbox>
                                     </li>
-                                    <li><a aria-label="product-category" rel="noreferrer" onClick={(e) => e.preventDefault()} href="#jacket"><span className="product-cb product-category-cb"></span>5 Bedroom</a>
-                                    {/* <span className="item-qty">12</span> */}
+                                    <li>
+                                    <Checkbox value="5BR">5 Bedroom</Checkbox>
                                     </li>
+                                    </Checkbox.Group>
                                 </ul>
                             </div>
+
+                            <Modal centered open={isConfirmSearchModalOpen} onOk={handleOkSearch} okText="Search" confirmLoading={filterSearchLoading} okType='secondary' onCancel={handleCancelSearch}>
+                            <Result title="Search for investment opportunities" subTitle="Please Note: 1 search = 1 token" />
+                            </Modal>
                        
                            
                             <div>
-                                <span className="shop-title relative font-serif font-medium text-darkgray block mb-[30px]">Filter By Sale Type </span>
-                                <div className="tag-cloud d-inline-block margin-10px-top">
+                                {/* <span className="shop-title relative font-serif font-medium text-darkgray block mb-[30px]"> </span> */}
+                                <Button size="large" loading={filterSearchLoading} onClick={showConfirmModal} className="w-full">
+                                  Search
+                                </Button>
+                                {/* <Buttons ariaLabel="filter" onClick={filterProperties} type="submit" className="btn-fill btn-fancy w-full font-medium font-serif rounded-none uppercase md:mb-[15px] sm:mb-0 mb-4" themeColor="#3eb489" color="#fff" size="md" title={`Search`} /> */}
+                                {/* <Buttons ariaLabel="reset" type="submit" className="btn-fill btn-fancy w-full font-medium font-serif rounded-none uppercase md:mb-[15px] sm:mb-0" themeColor="#70161e" color="#fff" size="md" title="Reset" /> */}
+                                {/* <div className="tag-cloud d-inline-block margin-10px-top">
                                     <Link aria-label="product-tags-link" to="#">Off Plan</Link>
                                     <Link aria-label="product-tags-link" to="#">Occupation Ready</Link>
-                                </div>
+                                </div> */}
                             </div>
                         </div>
                     </Row>
