3eda1bd344d6364323bb9d996c728820ed38488b
Brian <brian.wahinya.njau@huawei.com>
2024-03-01 16:06:10 +0300

    Area Data

--- src/Components/StatisticCard.jsx
+++ src/Components/StatisticCard.jsx
@@ -1,16 +1,18 @@
-import { Card } from 'antd'
-import React from 'react'
+import { Card } from "antd";
+import React from "react";
 
-const StatisticCard = ({title, value}) => {
+const StatisticCard = ({ title, value }) => {
   return (
-    <Card className='bg-[#08415c]' bordered={false}>
-    <div className=''>
-    <span className='font-base text-[#f3efe0] text-md mb-1'>{title}</span>
-    <br /> 
-    {/* <hr className='text-[#f3efe0]' /> */}       
-    <span className='font-semibold text-[#3eb489] text-[1.8rem] '>{value}</span>
-    </div> 
-    {/* <Statistic
+    <Card className="bg-[#08415c]" bordered={false}>
+      <div className="">
+        <span className="font-base text-[#f3efe0] text-md mb-1">{title}</span>
+        <br />
+        {/* <hr className='text-[#f3efe0]' /> */}
+        <span className="font-semibold text-[#3eb489] text-[1.5rem] ">
+          {value}
+        </span>
+      </div>
+      {/* <Statistic
     title="Average of Market Price"
     
     value={avgMarket||0}
@@ -23,7 +25,7 @@ const StatisticCard = ({title, value}) => {
     suffix=""
     /> */}
     </Card>
-  )
-}
+  );
+};
 
-export default StatisticCard
+export default StatisticCard;
