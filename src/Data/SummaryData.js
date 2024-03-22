58b89a0232695bff22d8be1affad5c8549f93563
Brian <brian.wahinya.njau@huawei.com>
2024-03-22 15:25:43 +0300

    Area data features

--- src/Data/SummaryData.js
+++ src/Data/SummaryData.js
@@ -1,32 +1,32 @@
 export const summaryData = {
-    kileleshwa: {
-        number: 26,
-        units:4260,
-        accerage:17.117,
-        price:73351640124
-    },
-    westands: {
-        number: 14,
-        units:2431,
-        accerage:5.639,
-        price:28465728333
-    },
-    lavington: {
-        number: 5,
-        units:1317,
-        accerage:3.21,
-        price:12429000000
-    },
-    kilimani: {
-        number: 34,
-        units:8242,
-        accerage:22.335,
-        price:95245502750
-    },
-    riverside: {
-        number: 8,
-        units:1138,
-        accerage:7.285,
-        price:22677422879
-    }
-}
+  kileleshwa: {
+    number: 33,
+    units: 5323,
+    accerage: 17.117,
+    price: 91753103458,
+  },
+  westands: {
+    number: 20,
+    units: 4285,
+    accerage: 5.639,
+    price: 49131695000,
+  },
+  lavington: {
+    number: 6,
+    units: 1409,
+    accerage: 3.21,
+    price: 13671366667,
+  },
+  kilimani: {
+    number: 42,
+    units: 9602,
+    accerage: 22.335,
+    price: 122151772750,
+  },
+  riverside: {
+    number: 9,
+    units: 1228,
+    accerage: 7.285,
+    price: 25148593333,
+  },
+};
