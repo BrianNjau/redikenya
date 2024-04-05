1679cb97816e8e365990298005f262d90647dfd2
Brian <brian.wahinya.njau@huawei.com>
2024-04-05 16:13:48 +0300

    Landing page summary data corrections

--- src/Data/SummaryData.js
+++ src/Data/SummaryData.js
@@ -2,31 +2,31 @@ export const summaryData = {
   kileleshwa: {
     number: 33,
     units: 5323,
-    accerage: 17.117,
+    accerage: 23.47,
     price: 91753103458,
   },
   westands: {
     number: 20,
     units: 4285,
-    accerage: 5.639,
+    accerage: 9.99,
     price: 49131695000,
   },
   lavington: {
     number: 6,
     units: 1409,
-    accerage: 3.21,
+    accerage: 4.001,
     price: 13671366667,
   },
   kilimani: {
     number: 42,
     units: 9602,
-    accerage: 22.335,
+    accerage: 31.724,
     price: 122151772750,
   },
   riverside: {
     number: 9,
     units: 1228,
-    accerage: 7.285,
+    accerage: 6.051,
     price: 25148593333,
   },
 };
