e35c0a146381a3a549ecfd6bf1200a08188239c1
Brian <brian.njau@strathmore.edu>
2024-05-14 17:32:18 +0300

    User Dashboard Cards

--- src/Components/UserDashLayout.jsx
+++ src/Components/UserDashLayout.jsx
@@ -1,14 +1,10 @@
 import React, { useContext, useEffect } from "react";
-import {
-  UploadOutlined,
-  UserOutlined,
-  VideoCameraOutlined,
-  CompassOutlined,
-} from "@ant-design/icons";
-import { Avatar, Image, Layout, Menu, theme } from "antd";
+import { CompassOutlined, PoweroffOutlined } from "@ant-design/icons";
+import { Avatar, ConfigProvider, Image, Layout, Menu, theme } from "antd";
 import LogoWhite from "../Assets/img/lightL.png";
 import { GlobalContext, useSupabaseAuth } from "../Context/Context";
 import { Link, useNavigate } from "react-router-dom";
+import { Supabase } from "../Functions/SupabaseClient";
 const { Header, Content, Footer, Sider } = Layout;
 
 const UserDashLayout = ({ children }) => {
@@ -28,108 +24,139 @@ const UserDashLayout = ({ children }) => {
     userMeta = user_metadata;
   }
 
-  //   const items = [
-  //     UserOutlined,
-  //     VideoCameraOutlined,
-  //     UploadOutlined,
-  //     UserOutlined,
-  //   ].map((icon, index) => ({
-  //     key: String(index + 1),
-  //     icon: React.createElement(icon),
-  //     label: `nav ${index + 1}`,
-  //   }));
-
   const items = [
     {
       key: "1",
       icon: React.createElement(CompassOutlined),
       label: <Link to="/user-dashboard">Dashboard</Link>,
     },
+    {
+      key: "2",
+      icon: React.createElement(CompassOutlined),
+      label: <Link to="/billing">Billing</Link>,
+    },
+    {
+      key: "3",
+      icon: React.createElement(CompassOutlined),
+      label: <Link to="/profile">Profile</Link>,
+    },
+    {
+      type: "divider",
+    },
+    {
+      key: "5",
+      icon: React.createElement(PoweroffOutlined),
+      label: <button onClick={Logout}>Logout</button>,
+    },
   ];
+  async function Logout() {
+    try {
+      //
+      //   setLoading(true);
+      const { error } = await Supabase.auth.signOut();
+
+      if (error) console.log(error);
+      //   setLoading(false);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   return (
-    <Layout>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div className="demo-logo-vertical mt-2 mb-16">
-          <Link to="/">
-            {" "}
-            <Image
-              preview={false}
-              className="default-logo p-1"
-              loading="lazy"
-              src={LogoWhite}
-              width={180}
-              alt="logo"
-            />
-          </Link>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["1"]}
-          items={items}
-        />
-      </Sider>
+    <ConfigProvider
+      theme={{
+        components: {
+          Menu: {
+            colorPrimary: "#3eb489",
+          },
+        },
+      }}
+    >
       <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
+        <Sider
+          breakpoint="lg"
+          collapsedWidth="0"
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
           }}
         >
-          <div className="float-right mr-4 ">
-            <button>
-              <Avatar
-                style={{
-                  backgroundColor: "#08415c",
-                  color: "#3EB489",
-                }}
-                className="mr-2"
-              >
-                {userMeta.fullName.split(" ")[0][0]}
-              </Avatar>
-              {userMeta.fullName}
-            </button>
+          <div className="demo-logo-vertical mt-2 mb-16">
+            <Link to="/">
+              {" "}
+              <Image
+                preview={false}
+                className="default-logo p-1"
+                loading="lazy"
+                src={LogoWhite}
+                width={180}
+                alt="logo"
+              />
+            </Link>
           </div>
-        </Header>
-        <Content
-          style={{
-            margin: "36px 24px 0",
-          }}
-        >
-          <div
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={["1"]}
+            items={items}
+          />
+        </Sider>
+        <Layout>
+          <Header
             style={{
-              padding: 24,
-              minHeight: "82.2vh",
-              height: "100%",
+              padding: 0,
               background: colorBgContainer,
-              borderRadius: borderRadiusLG,
             }}
           >
-            {children}
-          </div>
-        </Content>
-        <Footer
-          style={{
-            textAlign: "center",
-          }}
-        >
-          PDI Marketplace Kenya ©{new Date().getFullYear()} All rights reserved
-        </Footer>
+            <div className="float-right mr-4 ">
+              <button>
+                <Avatar
+                  style={{
+                    backgroundColor: "#08415c",
+                    color: "#3EB489",
+                  }}
+                  className="mr-2"
+                >
+                  {userMeta.fullName.split(" ")[0][0]}
+                </Avatar>
+                {userMeta.fullName}
+              </button>
+            </div>
+          </Header>
+          <Content
+            style={{
+              margin: "36px 24px 0",
+            }}
+          >
+            <div
+              style={{
+                padding: 24,
+                minHeight: "82.2vh",
+                height: "100%",
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+              }}
+            >
+              {children}
+            </div>
+          </Content>
+          <Footer
+            style={{
+              textAlign: "center",
+            }}
+          >
+            PDI Marketplace Kenya ©{new Date().getFullYear()} All rights
+            reserved
+          </Footer>
+        </Layout>
       </Layout>
-    </Layout>
+    </ConfigProvider>
   );
 };
 
--- src/Pages/UserDashboard.jsx
+++ src/Pages/UserDashboard.jsx
@@ -1,10 +1,108 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserDashLayout from "../Components/UserDashLayout";
+import { Card, Col, Row, Statistic } from "antd";
+import { useSupabaseAuth } from "../Context/Context";
+import { Link, useNavigate } from "react-router-dom";
+import { summaryData } from "../Data/SummaryData";
 
 const UserDashboard = () => {
+  const session = useSupabaseAuth();
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!session) {
+      navigate("/login");
+    }
+  }, [session]);
+
+  let userMeta;
+  if (session) {
+    const { user_metadata } = session.user;
+    userMeta = user_metadata;
+  }
+
   return (
     <UserDashLayout>
-      <div>Hello home page</div>
+      <h6 className=" font-medium text-darkgray -tracking-[1px]">
+        Welcome back,&ensp;
+        {/* <span className="text-gradient bg-gradient-to-tr from-[#ff6052] to-[#ff367c] font-semibold"></span>{" "} */}
+        <span className="text-gradient bg-gradient-to-tr from-[#3EB489] to-[#08415c] font-semibold">
+          {userMeta.fullName.split(" ")[0]}
+        </span>
+      </h6>
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col span={8}>
+          <Card bordered={true}>
+            <Statistic
+              title="Available tokens"
+              value={0}
+              //   precision={2}
+              //   valueStyle={{
+              //     color: "#3f8600",
+              //   }}
+              //   prefix={<ArrowUpOutlined />}
+              //   suffix="%"
+            />
+            <button
+              aria-label="subscribe"
+              type="submit"
+              className={`text-xs py-[12px] px-[28px] uppercase`}
+            >
+              Recharge
+            </button>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card bordered={true}>
+            <Statistic
+              title="Property Units"
+              value={Object.values(summaryData).reduce(
+                (acc, curr) => (acc = acc + curr["units"]),
+                0
+              )}
+            />
+            <Link to="/search">
+              <button
+                aria-label="subscribe"
+                type="submit"
+                className={`text-xs py-[12px] px-[28px] uppercase`}
+              >
+                Search
+              </button>
+            </Link>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card bordered={true}>
+            <Statistic
+              title="Active Subscription"
+              value={"No Active Plan"}
+              //   precision={2}
+              //   valueStyle={{
+              //     color: "#cf1322",
+              //   }}
+              //   prefix={<ArrowDownOutlined />}
+              //   suffix="%"
+            />
+            <button
+              aria-label="subscribe"
+              type="submit"
+              className={`text-xs py-[12px] px-[28px] uppercase`}
+            >
+              Change
+            </button>
+          </Card>
+        </Col>
+      </Row>
+
+      {/* If user is logging in for the first time */}
     </UserDashLayout>
   );
 };
